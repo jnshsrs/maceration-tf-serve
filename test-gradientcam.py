5f932ae480b7f112ed928b27404af5570ad49ff1
Your Name <you@example.com>
2022-06-22 22:07:19 +0200

    added test scripts and added images (not original but created by a GAN)

---
+++ test-gradientcam.py
@@ -0,0 +1,45 @@
+import requests
+
+url = "http://127.0.0.1:5000/image/gradcam"
+
+payload = {}
+headers = {}
+
+files = [
+    (
+        "image",
+        (
+            "maceration-present-48393.png",
+            open(
+                "/Users/jenshusers/Downloads/ziel-tensorflow-app/images/maceration-present-48393.png",
+                "rb",
+            ),
+            "image/png",
+        ),
+    )
+]
+
+response = requests.request("POST", url, headers=headers, data=payload, files=files)
+
+print("Prediction for DFU with maceration")
+print(response.text)
+
+
+files = [
+    (
+        "image",
+        (
+            "maceration-absent-46478.png",
+            open(
+                "/Users/jenshusers/Downloads/ziel-tensorflow-app/images/maceration-absent-46478.png",
+                "rb",
+            ),
+            "image/png",
+        ),
+    )
+]
+
+response = requests.request("POST", url, headers=headers, data=payload, files=files)
+
+print("Prediction for DFU without maceration")
+print(response.text)
---
+++ test-prediction.py
@@ -0,0 +1,45 @@
+import requests
+
+url = "http://127.0.0.1:5000/image/predict"
+
+payload = {}
+headers = {}
+
+files = [
+    (
+        "image",
+        (
+            "maceration-present-48393.png",
+            open(
+                "/Users/jenshusers/Downloads/ziel-tensorflow-app/images/maceration-present-48393.png",
+                "rb",
+            ),
+            "image/png",
+        ),
+    )
+]
+
+response = requests.request("POST", url, headers=headers, data=payload, files=files)
+
+print("Prediction for DFU with maceration")
+print(response.text)
+
+
+files = [
+    (
+        "image",
+        (
+            "maceration-absent-0497.png",
+            open(
+                "/Users/jenshusers/Downloads/ziel-tensorflow-app/images/maceration-absent-0497.png",
+                "rb",
+            ),
+            "image/png",
+        ),
+    )
+]
+
+response = requests.request("POST", url, headers=headers, data=payload, files=files)
+
+print("Prediction for DFU without maceration")
+print(response.text)
